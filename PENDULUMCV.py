dc1e7d7c7ff003cdbabc4f22976eb38beed30728
ChebrecChebrecov <138617855+ChebrecChebrecov@users.noreply.github.com>
2026-09-18 00:27:34 +0300

    Major optimization improvements

--- PENDULUMCV.py
+++ PENDULUMCV.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from scipy.fft import fft, fftfreq
-print("Enter the path of the folder, containing the object. For example: images/ball/sample/ to analyze images/ball/sample/black_ball")
+print("Enter the path of the object. For example: images/ball/sample/black_ball.jpg")
 sample_path = input()
 print("Enter the path of the video footage. For example:videos/pendulum/main_test.mp4")
 video_path = input()
+ball_center = 0
+orb = cv2.ORB_create()
+img2 = cv2.imread(sample_path, 0)
+kp2, des2 = orb.detectAndCompute(img2, None)
 def freq_fft(points, fps):
     clean = []
     for p in points:
@@ -42,20 +46,15 @@ def find_coordinates_of_ball(cnts, image):
     return ball_coordinates
 def find_features(img1):
     correct_matches_dct = {}
-    directory = sample_path
-    for image in os.listdir(directory):
-        img2 = cv2.imread(directory+image, 0)
-        orb = cv2.ORB_create()
-        kp1, des1 = orb.detectAndCompute(img1, None)
-        kp2, des2 = orb.detectAndCompute(img2, None)
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des1, des2, k=2)
-        correct_matches = []
-        for m, n in matches:
-            if m.distance < 0.75*n.distance:
-                correct_matches.append([m])
-        correct_matches_dct[image.split('.')[0]]= len(correct_matches)
-        correct_matches_dct = dict(sorted(correct_matches_dct.items(), key=lambda item: item[1], reverse=True))
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    correct_matches = []
+    for m, n in matches:
+        if m.distance < 0.75*n.distance:
+            correct_matches.append([m])
+    correct_matches_dct["object".split('.')[0]]= len(correct_matches)
+    correct_matches_dct = dict(sorted(correct_matches_dct.items(), key=lambda item: item[1], reverse=True))
     return list(correct_matches_dct.keys())[0]
 def video_to_frames(path, folder):
     cap = cv2.VideoCapture(path)
@@ -77,11 +76,9 @@ def video_to_frames(path, folder):
         gray_main_image = cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
         contours = find_contours_of_ball(gray_main_image)
         ball_location = find_coordinates_of_ball(contours, gray_main_image)
-        ball_center = 0
         for key, value in ball_location.items():
             ball_center = (value[0]+value[2])/2
         points.append(ball_center)
-        print(ball_center)
     fps = cap.get(cv2.CAP_PROP_FPS)
     cap.release()
     mean = sum(points)/frame_count
